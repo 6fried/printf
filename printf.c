10ab9721ca66996bd36952674d129b287741a236
preshchima <preshchima@gmail.com>
2022-11-21 09:00:39 -0800

    conversion specifier %d

--- printf.c
+++ printf.c
@@ -1,4 +1,6 @@
-
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - function
@@ -11,7 +13,8 @@ int _printf(const char *format, ...)
 
 	va_start(arg, format);
 
-	int i;
+	int i, j;
+	signed int n
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
@@ -38,6 +41,29 @@ int _printf(const char *format, ...)
 				write(1, "%%", 1);
 				break;
 			case 'd':
+				n = va_arg(arg, int);
+				if (n < 0)
+				{
+					n = -n;
+					write(1, "-", 1);
+				}
+				static char rep[] = "0123456789ABCDEF";
+				static char buffer[50];
+				char *ptr;
+
+				ptr = &buffer[49];
+				*ptr = '\0';
+
+				do
+				{
+					*--ptr = rep[n%10];
+					n /= 10;
+				}while(n != 0);
+
+				for (j = 0; ptr[j] != '\0'; j++)
+				{
+					write(1, &ptr[j], 1);
+				}
 				break;
 			case 'i':
 				break;
