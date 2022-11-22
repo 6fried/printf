4ca03681f0e7986591e56c5e87d40252735791e5
6fried <kellyhantan@gmail.com>
2022-11-22 21:38:40 +0100

    First working exectabe

--- main.h
+++ main.h
@@ -2,5 +2,10 @@
 #define PRINTF_H
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *str);
+char *dectostr(int n);
 #endif
--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 
 /**
  * _printf - function
@@ -9,68 +8,41 @@
  */
 int _printf(const char *format, ...)
 {
+	int i, n = 0;
 	va_list arg;
 
 	va_start(arg, format);
-
-	int i, j;
-	signed int n
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
-
 			switch (format[i])
 			{
 			case 'c':
-				char c = va_arg(arg, int);
-
-				write(1, &c, 1);
+				n += _putchar(va_arg(arg, int));
 				break;
 			case 's':
-				int j;
-
-				char *str = va_arg(arg, char *);
-
-				for (j = 0; str[j] != '\0'; j++)
-					write(1, &str[j], 1);
+				n += _puts(va_arg(arg, char *));
 				break;
 			case '%':
-				write(1, "%%", 1);
+				n += _putchar('%');
 				break;
 			case 'd':
-				n = va_arg(arg, int);
-				if (n < 0)
-				{
-					n = -n;
-					write(1, "-", 1);
-				}
-				static char rep[] = "0123456789ABCDEF";
-				static char buffer[50];
-				char *ptr;
-
-				ptr = &buffer[49];
-				*ptr = '\0';
-
-				do
-				{
-					*--ptr = rep[n%10];
-					n /= 10;
-				}while(n != 0);
-
-				for (j = 0; ptr[j] != '\0'; j++)
-				{
-					write(1, &ptr[j], 1);
-				}
+				n += _puts(dectostr(va_arg(arg, int)));
 				break;
 			case 'i':
-				break;
-			default:
+				n += _puts(dectostr(va_arg(arg, int)));
 				break;
 			}
 		}
+		else
+		{
+			_putchar(format[i]);
+			n++;
+		}
 		va_end(arg);
 	}
+	return (n);
 }
