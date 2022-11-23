83d1ccc9780e2a7739b532597dffe640adbfcc08
6fried <kellyhantan@gmail.com>
2022-11-23 10:21:40 +0100

    .

--- printf.c
+++ printf.c
@@ -9,43 +9,20 @@
 int _printf(const char *format, ...)
 {
 	int i, n = 0;
-	va_list arg;
+	va_list ap;
 
-	va_start(arg, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
+
+	va_start(ap, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-		{
-			i++;
-			switch (format[i])
-			{
-			case 'c':
-				n += _putchar(va_arg(arg, int));
-				break;
-			case 's':
-				n += _puts(va_arg(arg, char *));
-				break;
-			case '%':
-				n += _putchar('%');
-				break;
-			case ' d':
-				n += _puts(basetostr(va_arg(arg, int), 10));
-				break;
-			case 'i':
-				n += _puts(basetostr(va_arg(arg, int), 10));
-				break;
-			default:
-				break;
-			}
-		}
+			n += handle_flag(format[++i], ap);
 		else
-		{
-			_putchar(format[i]);
-			n++;
-		}
-		va_end(arg);
+			n += _putchar(format[i]);
+		va_end(ap);
 	}
 	return (n);
 }
-
